4cc1ce566d15ebf6b289d9c2479ad184b6b35696
Caio Zanqueta <81552311+CaioZanqueta@users.noreply.github.com>
2023-11-05 03:32:41 -0300

    feat: add login and form

--- src/app/pages/login/Login.tsx
+++ src/app/pages/login/Login.tsx
@@ -1,10 +1,32 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 export const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleEntrar = () => {
+    console.log(email)
+    console.log(password)
+  }
 
   return (
     <div>
-      <p>Login</p>
+      <form>
+        <label>
+          <span>Email</span>
+          <input type="text" value={email} onChange={e => setEmail(e.target.value)}/>
+        </label>
+
+        <label>
+          <span>Senha</span>
+          <input type="password" value={password} onChange={e => setPassword(e.target.value)}/>
+        </label>
+
+        <button type="button" onClick={handleEntrar}>
+          Entrar
+        </button>
+      </form>
 
       <Link to="/dashboard">Página Inicial</Link>
     </div>
