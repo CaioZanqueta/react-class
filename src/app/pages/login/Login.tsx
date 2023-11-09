8e3724070368fdd9151d4cb832a5ae7f917a5381
Caio Zanqueta <81552311+CaioZanqueta@users.noreply.github.com>
2023-11-09 16:29:27 -0300

    login

--- src/app/pages/login/Login.tsx
+++ src/app/pages/login/Login.tsx
@@ -1,9 +1,20 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 export const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+
+  useEffect(() => {
+    if (window.confirm('Você é homem?')) {
+      console.log('Homem')
+    }
+  }, []);
+
+  useEffect(() => {
+    console.log(email)
+    console.log(password)
+  }, [email, password]);
 
   const handleEntrar = () => {
     console.log(email)
